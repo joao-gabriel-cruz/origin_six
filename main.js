c3bfe9f9e8bd06df42a891d52bdecedf00a29643
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-04-28 16:35:53 -0300

    _

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle ')
+const toggle = document.querySelectorAll('nav .toggle')
 
-for (const elemet of toggle) {
-  elemet.addEventListener('click', () => {
+for (const element of toggle) {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
 
-const header = document.querySelector('#header')
-const navHEight = header.offsetHeight
-
-function changerHeaderWhenScroll() {
-  if (window.scrollY >= navHEight) {
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll')
-  }
-}
+/* quando clicar em um item do menu, esconder o menu */
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', () => {
+  link.addEventListener('click', function () {
     nav.classList.remove('show')
   })
 }
 
-/*=== swiper === */
-const swiper = new Swiper('.swiper', {
-  slidePerView: 1,
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
-
   mousewheel: true,
-
   keyboard: true,
-
   breakpoints: {
     767: {
       slidesPerView: 2,
@@ -45,46 +47,44 @@ const swiper = new Swiper('.swiper', {
   }
 })
 
-/* ==== scrollReaveal ==== */
-const scrollReaveal = ScrollReveal({
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
-  durantion: 700,
+  duration: 700,
   reset: true
 })
 
-scrollReaveal.reveal(
-  `
-#home .image, #home .text,
-#about .image, #about .text,
-#service header, #service .card, 
-#testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links,
-footer .brand, footer .social
-`,
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
   { interval: 100 }
 )
 
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-  if (window.scrollY > 560) {
+  if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
   }
 }
 
-/* menu ativo conforme a rolagem */
-
+/* Menu ativo conforme a seção visível na página */
 const sections = document.querySelectorAll('main section[id]')
-
-function activeMenuCurrentSection() {
+function activateMenuAtCurrentSection() {
   const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
   for (const section of sections) {
     const sectionTop = section.offsetTop
     const sectionHeight = section.offsetHeight
-
     const sectionId = section.getAttribute('id')
 
     const checkpointStart = checkpoint >= sectionTop
@@ -92,18 +92,19 @@ function activeMenuCurrentSection() {
 
     if (checkpointStart && checkpointEnd) {
       document
-        .querySelector('nav ul li a[href*= ' + sectionId + ']')
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
         .classList.add('active')
     } else {
       document
-        .querySelector('nav ul li a[href*= ' + sectionId + ']')
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
         .classList.remove('active')
     }
   }
 }
 
-window.addEventListener('scroll', () => {
-  activeMenuCurrentSection()
-  changerHeaderWhenScroll()
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
